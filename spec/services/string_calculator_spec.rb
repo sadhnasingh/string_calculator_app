1db5ac42500348288d610de3319484039fa1a697
Sadhna Singh <sadhnasingh642@gmail.com>
2025-10-06 17:55:42 +0530

    TDD: add failing test - single number

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe StringCalculator, type: :service do
     it 'returns 0 for empty string' do
       expect(StringCalculator.new.add('')).to eq 0
     end
+
+    it 'returns the number for a single number' do
+      expect(StringCalculator.new.add('1')).to eq 1
+    end
   end
 end
