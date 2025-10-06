665228b25b79a468a49685e37f99263be0a94843
Sadhna Singh <sadhnasingh642@gmail.com>
2025-10-06 18:18:14 +0530

    TDD: implement single-number parsing

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -1,6 +1,7 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.nil? || numbers == ''
+    return numbers.to_i if numbers =~ /\A-?\d+\z/
     raise NotImplementedError
   end
 end
