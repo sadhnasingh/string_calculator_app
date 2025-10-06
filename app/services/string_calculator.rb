c21ceb2fc04c1baa89ceb08ccd118bd526aaf66c
Sadhna Singh <sadhnasingh642@gmail.com>
2025-10-06 18:41:47 +0530

    TDD: implement comma-separated sum

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -2,6 +2,6 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.nil? || numbers == ''
     return numbers.to_i if numbers =~ /\A-?\d+\z/
-    raise NotImplementedError
+    numbers.split(',').map(&:to_i).sum
   end
 end
