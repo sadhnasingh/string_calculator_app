293f2e8e13483b971d4edf159fec66f3c9b6e455
Sadhna Singh <sadhnasingh642@gmail.com>
2025-10-06 18:41:01 +0530

    TDD: add failing test - two comma numbers

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -9,5 +9,9 @@ RSpec.describe StringCalculator, type: :service do
     it 'returns the number for a single number' do
       expect(StringCalculator.new.add('1')).to eq 1
     end
+
+    it 'adds two comma-separated numbers' do
+      expect(StringCalculator.new.add('1,2')).to eq 3
+    end
   end
 end
