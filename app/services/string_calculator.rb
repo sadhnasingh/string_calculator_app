f15071d0af821de703d49f648ae41337680f4253
Sadhna Singh <sadhnasingh642@gmail.com>
2025-10-07 11:50:05 +0530

    TDD: detect negatives and raise listing them

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -2,14 +2,27 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.nil? || numbers == ''
     return numbers.to_i if numbers =~ /\A-?\d+\z/
+    
     str = numbers.dup
     delimiters = [',', "\n"]
+
     if str.start_with?('//')
       header, str = str.split("\n", 2)
-      delimiters << header[2..-1]
+      custom = header[2..-1]
+      if custom.start_with?('[')
+        delimiters += custom.scan(/\[(.+?)\]/).flatten
+      else
+        delimiters << custom
+      end
     end
 
     regex = Regexp.union(*delimiters.map { |d| Regexp.new(Regexp.escape(d)) })
-    str.split(regex).reject(&:empty?).map(&:to_i).sum
+    tokens = str.split(regex).reject(&:empty?)
+    ints = tokens.map(&:to_i)
+
+    negatives = ints.select { |n| n < 0 }
+    raise ArgumentError, "Negatives not allowed: #{negatives.join(',')}" unless negatives.empty?
+
+    ints.reject { |n| n > 1000 }.sum
   end
 end
