ab6c4d8270683b3d7f7a35ecd3ea11c25fb0a9a3
Sadhna Singh <sadhnasingh642@gmail.com>
2025-10-07 11:28:54 +0530

    TDD: support newline as delimiter

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -2,6 +2,6 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.nil? || numbers == ''
     return numbers.to_i if numbers =~ /\A-?\d+\z/
-    numbers.split(',').map(&:to_i).sum
+    numbers.split(/,|\n/).map(&:to_i).sum
   end
 end
