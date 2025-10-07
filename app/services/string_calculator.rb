8db485033a697be1e793f4824b8d4f6575479bac
Sadhna Singh <sadhnasingh642@gmail.com>
2025-10-07 11:44:25 +0530

    TDD: support custom single-char delimiter

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -2,6 +2,14 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.nil? || numbers == ''
     return numbers.to_i if numbers =~ /\A-?\d+\z/
-    numbers.split(/,|\n/).map(&:to_i).sum
+    str = numbers.dup
+    delimiters = [',', "\n"]
+    if str.start_with?('//')
+      header, str = str.split("\n", 2)
+      delimiters << header[2..-1]
+    end
+
+    regex = Regexp.union(*delimiters.map { |d| Regexp.new(Regexp.escape(d)) })
+    str.split(regex).reject(&:empty?).map(&:to_i).sum
   end
 end
