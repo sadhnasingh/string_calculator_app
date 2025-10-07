6b41ceec5ada033ce5eafdd03779477a2821b397
Sadhna Singh <sadhnasingh642@gmail.com>
2025-10-07 11:44:59 +0530

    TDD: add failing test - negatives should raise

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -22,5 +22,13 @@ RSpec.describe StringCalculator, type: :service do
       expect(StringCalculator.new.add("1\n2,3")).to eq 6
     end
 
+    it 'supports custom delimiter like //;\\n1;2' do
+      expect(StringCalculator.new.add("//;\n1;2")).to eq 3
+    end
+
+    it 'raises when negatives are present and lists them' do
+      expect { StringCalculator.new.add("1,-2,-5") }.to raise_error(ArgumentError, "Negatives not allowed: -2,-5")
+    end
+
   end
 end
