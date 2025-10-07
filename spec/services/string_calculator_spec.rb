88cdc1ef7761ce3b47ac1c0de1f5b606e7456c5b
Sadhna Singh <sadhnasingh642@gmail.com>
2025-10-07 11:27:09 +0530

    TDD: add failing test - newline delimiter

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -18,5 +18,9 @@ RSpec.describe StringCalculator, type: :service do
       expect(StringCalculator.new.add('1,2,3,4')).to eq 10
     end
 
+    it 'accepts newlines between numbers' do
+      expect(StringCalculator.new.add("1\n2,3")).to eq 6
+    end
+
   end
 end
